67f949a6a5b116608007fb55b87b4efe8a606541
Igor Santos <igor.sagitarius@gmail.com>
2021-08-15 17:46:37 -0300

    Começo da implementação das funcionalidades

---
+++ script.js
@@ -0,0 +1,8 @@
+function inserirGorjeta(num){
+  let conta = document.getElementById('conta');
+  let pessoas = document.getElementById('pessoas');
+  let a = Number(conta.value)
+  let res = document.getElementById('resGorjeta')
+  res.innerHTML = num;
+
+}
