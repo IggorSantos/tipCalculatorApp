0580c8f0739608b146c3d91cb179ce3e16085ecf
Igor Santos <igor.sagitarius@gmail.com>
2021-08-15 18:22:50 -0300

    Realizando calculo do valor por pessoa

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ function inserirGorjeta(num){
   let conta = Number(document.getElementById('conta').value);
   let pessoas = Number(document.getElementById('pessoas').value);
   conta += conta * (num/100)
-  let res = document.getElementById('resGorjeta')
-  res.innerHTML = conta;
+  let valorPessoa = conta / pessoas
+  let res = document.getElementById('resConta')
+  res.innerHTML = valorPessoa.toFixed(2);
 
 }
