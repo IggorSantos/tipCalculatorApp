f995f3c4bbae6cc5266bef173e84c190a287e12e
Igor Santos <igor.sagitarius@gmail.com>
2021-08-17 18:20:15 -0300

    Função realizando a conta

--- script.js
+++ script.js
@@ -17,6 +17,15 @@ function inserirGorjeta(num){
 function inserirGorgetakeydown(){
   if(event.key === "Enter"){
     let num = Number(document.getElementById('gorjeta').value);
-    alert(num)
+    let conta = Number(document.getElementById('conta').value);
+    let pessoas = Number(document.getElementById('pessoas').value);
+    let valorGorjeta = conta * (num/100)
+    conta += valorGorjeta;
+    let valorPessoa = conta / pessoas;
+    let gorjetaPessoa = valorGorjeta / pessoas;
+    let resConta = document.getElementById('resConta')
+    let resGorjeta = document.getElementById('resGorjeta')
+    resConta.innerHTML = valorPessoa.toFixed(2);
+    resGorjeta.innerHTML = gorjetaPessoa.toFixed(2);
   }
 }
