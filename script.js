7421269391c8f5a5ec61b6b939d22db670aecb09
Igor Santos <igor.sagitarius@gmail.com>
2021-08-20 17:20:53 -0300

    Correção na função que pega o valor digitado pelo usuário

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 //Pegar o valor customizado e resetar os valores quando clicar em resetar
-document.getElementById('gorjeta').addEventListener("keypress",inserirGorgetakeydown);
+document.getElementById('gorjeta').addEventListener("keypress",inserirGorgetaKeydown);
 document.getElementById('resetar').addEventListener("click",resetar);
 
 function inserirGorjeta(num){
   let conta = parseFloat(document.getElementById('conta').value);
   let pessoas = Number(document.getElementById('pessoas').value);
-  if(isNaN(conta) || isNaN(pessoas)){
+  if(pessoas == 0){
     let erro = document.getElementById('erro')
     erro.innerHTML = ("Não pode ser zero")
+    erro.style.color = 'red';
   }else{
     let valorGorjeta = conta * (num/100)
     conta += valorGorjeta;
@@ -21,21 +22,21 @@ function inserirGorjeta(num){
 
 }
 
-function inserirGorgetakeydown(){
+function inserirGorgetaKeydown(){
   if(event.key === "Enter"){
-    let num = Number(document.getElementById('gorjeta').value);
-    let conta = Number(document.getElementById('conta').value);
-    let pessoas = Number(document.getElementById('pessoas').value);
-    let valorGorjeta = conta * (num/100)
-    conta += valorGorjeta;
-    let valorPessoa = conta / pessoas;
-    let gorjetaPessoa = valorGorjeta / pessoas;
-    let resConta = document.getElementById('resConta')
-    let resGorjeta = document.getElementById('resGorjeta')
-    resConta.innerHTML = valorPessoa.toFixed(2);
-    resGorjeta.innerHTML = gorjetaPessoa.toFixed(2);
+      let num = Number(document.getElementById('gorjeta').value);
+      let conta = parseFloat(document.getElementById('conta').value);
+      let pessoas = Number(document.getElementById('pessoas').value);
+      let valorGorjeta = conta * (num/100)
+      conta += valorGorjeta;
+      let valorPessoa = conta / pessoas;
+      let gorjetaPessoa = valorGorjeta / pessoas;
+      let resConta = document.getElementById('resConta')
+      let resGorjeta = document.getElementById('resGorjeta')
+      resConta.innerHTML = valorPessoa.toFixed(2);
+      resGorjeta.innerHTML = gorjetaPessoa.toFixed(2);
+    }
   }
-}
 
 function resetar(){
   document.getElementById('conta').value = '';
